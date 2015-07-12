d67f35c15c1ca1ad30a16e90cc1ca55966ef7f7f
Theunissen lab <theunissenlab@gmail.com>
2015-07-11 21:29:58 -0700

    small local change

--- pecking_analysis/objects.py
+++ pecking_analysis/objects.py
@@ -204,6 +204,7 @@ class HDF5Store(object):
             if group_name in group:
                 if ii == (len(group_names) - 1):
                     if overwrite:
+			del group[group_name]["data"]
                         del group[group_name]
                     else:
                         IOError("Block %s has already been imported into %s. To overwrite add overwrite=True" % (blk,
