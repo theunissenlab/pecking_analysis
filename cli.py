6941327153113fab09d0664d959d7fa2e8307014
kevin <thekevinyu@gmail.com>
2022-07-07 12:04:51 -0700

    Add command to count missing trials

--- cli.py
+++ cli.py
@@ -106,10 +106,69 @@ def package_data(output_path, debug):
     )
 
 
+@click.command()
+@click.option("--debug", is_flag=True)
+def missing_trials(debug):
+    """Count # of missing trials in original data by (by skipped numbers in 'trial' column of csvs)
+
+    The original recording sometimes missed errors in the csv files (missing trial number)
+
+    1. Count missing trials by # trials where trial(n)+1 != trial(n+1)  [before pipelines applied]
+    2. Count total trials [after pipelines applied]
+    3. Compute (output of 1 / output of 2)
+    """
+    import pandas as pd
+    import numpy as np
+
+    from configs.active_config import config
+    from analysis.pipelines.project_lesions_2021 import load_all_for_subject
+
+    if debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+    else:
+        logging.getLogger().setLevel(logging.INFO)
+
+    # Patch config so that there are no filters applied
+    config.filters = []
+
+    n_trials = 0
+    n_missing = 0
+    for subject in config.subjects:
+        # Preprocessing steps
+        df = load_all_for_subject(subject, config)
+
+        # Count missing trials by trials where the trial count increases by an amount
+        # more than 1
+        trial_diff = np.diff(df["Trial"])
+
+        subj_n_trials = len(df)
+        subj_n_missing = int(np.sum(trial_diff > 1))
+        missing_trials = np.where(trial_diff > 1)[0]
+
+        n_trials += subj_n_trials
+        n_missing += subj_n_missing
+
+        print("{subject}: Identified {n_missing} trials missing out of {n_trials}".format(
+            subject=subject,
+            n_missing=subj_n_missing,
+            n_trials=subj_n_trials
+        ))
+
+    print("-" * 40)
+    print("Identified {n_missing} trials missing out of {n_trials}".format(
+        n_missing=n_missing,
+        n_trials=n_trials
+    ))
+
+    print("Note: run `python cli.py data` and sum the trials column to get the total number of trials after filtering")
+
+
+
 cli.add_command(download_data)
 cli.add_command(summarize_data)
 cli.add_command(locate_data)
 cli.add_command(package_data)
+cli.add_command(missing_trials)
 
 
 if __name__ == "__main__":
